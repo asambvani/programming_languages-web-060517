5f6ae86624c86c866b0984006800a7a0d492d975
Alex Sambvani <alexsambvani@gmail.com>
2017-05-29 22:11:52 +0000

    Done.

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,3 +1,24 @@
 def reformat_languages(languages)
-  # your code here
+  hash = {}
+  languages_array = []
+  languages.each do |key1, value1|
+    value1.keys.each do |key|
+      languages_array << key
+    end
+  end
+  languages_array.uniq!
+  languages_array.each do |language|
+    hash[language] = {}
+    hash[language][:type] = ""
+    hash[language][:style] = []
+    languages.each do |key1, value1|
+      value1.each do |key2, value2|
+        if language == key2
+          hash[language][:type] = value2[:type]
+          hash[language][:style] << key1
+        end
+      end
+    end
+  end
+  return hash
 end
